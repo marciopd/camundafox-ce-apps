a48ed5bb0439296d78ef847e71d25dbe92a99daf
marciopd <marciopd@gmail.com>
2013-02-18 00:32:11 -0300

    Install script and correction groupId

---
+++ install/installCamundaFoxPlatform.sh
@@ -0,0 +1,16 @@
+echo '--------------------------------------'
+echo 'Camunda Fox CE Platform installer'
+echo 'https://github.com/marciopd/camundafox-ce-apps/README.md'
+echo 'This scripts installs a camunda fox CE platform in order to deploy and run the developed process applications.'
+echo '--------------------------------------'
+echo '1 - Creating appserver directory: ~/camunda-fox-tests-env/appserver'
+mkdir -p ~/camunda-fox-tests-env/appserver
+echo '--------------------------------------'
+echo '2 - Downloading Camunda Fox CE Platform'
+wget http://fox.camunda.com/community/release/fox-platform/jboss7.1/1.30.0/fox-platform-ce-jboss-1.30.0.zip -P ~/camunda-fox-tests-env/appserver
+echo '--------------------------------------'
+echo '3 - Unzipping...'
+unzip ~/camunda-fox-tests-env/appserver/fox-platform-ce-jboss-1.30.0.zip -d ~/camunda-fox-tests-env/appserver/
+echo '--------------------------------------'
+echo '3 - Running FOX CE application server'
+sh ~/camunda-fox-tests-env/appserver/fox-platform-ce-jboss-1.30.0/server/jboss-as-7.1.0.Final/bin/standalone.sh
