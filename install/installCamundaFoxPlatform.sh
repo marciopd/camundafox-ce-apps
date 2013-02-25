a493d399344c7ec3758a8f66badb640eae963943
marciopd <marciopd@gmail.com>
2013-02-25 17:32:11 -0300

    Updating camunda fox platform version.

--- install/installCamundaFoxPlatform.sh
+++ install/installCamundaFoxPlatform.sh
@@ -7,10 +7,10 @@ echo '1 - Creating appserver directory: ~/camunda-fox-tests-env/appserver'
 mkdir -p ~/camunda-fox-tests-env/appserver
 echo '--------------------------------------'
 echo '2 - Downloading Camunda Fox CE Platform'
-wget http://fox.camunda.com/community/release/fox-platform/jboss7.1/1.30.0/fox-platform-ce-jboss-1.30.0.zip -P ~/camunda-fox-tests-env/appserver
+wget http://fox.camunda.com/community/release/fox-platform/jboss7.1/1.32.0/fox-platform-ce-jboss-1.32.0.zip -P ~/camunda-fox-tests-env/appserver
 echo '--------------------------------------'
 echo '3 - Unzipping...'
-unzip ~/camunda-fox-tests-env/appserver/fox-platform-ce-jboss-1.30.0.zip -d ~/camunda-fox-tests-env/appserver/
+unzip ~/camunda-fox-tests-env/appserver/fox-platform-ce-jboss-1.32.0.zip -d ~/camunda-fox-tests-env/appserver/
 echo '--------------------------------------'
 echo '3 - Running FOX CE application server'
-sh ~/camunda-fox-tests-env/appserver/fox-platform-ce-jboss-1.30.0/server/jboss-as-7.1.0.Final/bin/standalone.sh
+sh ~/camunda-fox-tests-env/appserver/fox-platform-ce-jboss-1.32.0/server/jboss-as-7.1.0.Final/bin/standalone.sh
